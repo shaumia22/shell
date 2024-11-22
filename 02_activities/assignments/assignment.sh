292a5bd29c5494caa922244013e3920644add8a8
=Shaumia <=shaumiara@gmail.com>
2024-11-22 17:48:28 -0500

    updated corrections

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -36,7 +36,7 @@ cp -v raw/event*.log processed/event_logs/
 cp -v raw/user*.log processed/user_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm raw/ipaddr*
-rm processed/ipaddr*
+rm processed/user_logs/ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 ls ./processed > inventory.txt
 
